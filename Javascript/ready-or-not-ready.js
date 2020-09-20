f8ec0204c7de7b50175e605b13f2e2f1efde0d1c
Andrés Maldonado <contact.a.maldonado@gmail.com>
2020-09-20 19:50:09 -0300

    update

---
+++ Javascript/ready-or-not-ready.js
@@ -0,0 +1,8 @@
+// Porque jQuery es el diablo... Opciones a .ready()
+
+document.addEventListener("DOMContentLoaded", function(){
+    // Solo cuando el DOM se encuentra listo
+};
+window.onload = () => {
+    // Do something
+};
